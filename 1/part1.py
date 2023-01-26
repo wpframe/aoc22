2ef3f988c9a51d0bd63fb05ec42255379da4e04a
will <frawi873@student.otago.ac.nz>
2023-01-26 15:18:58 +1300

    cleaned up bcos bored

--- 1/part1.py
+++ 1/part1.py
@@ -1,15 +1,8 @@
-# Day 1 Part 1
-
-file = open("input.txt", "r")
-
-max = 0
-
-count = 0
+file = open("input.txt", "r") # Day 1 Part 1
+max, count = 0
 for line in file:
-    line = line.strip("\n")
-    if line == "":
-        if count > max: max = count
+    if line.strip("\n") == "": 
+        if count > max: max = count:
         count = 0
     else: count = count + int(line)
-        
 print(max)
--- 1/part2.py
+++ 1/part2.py
@@ -1,15 +1,9 @@
-# Day 1 Part 2
-
-file = open("input.txt", "r")
-
+file = open("input.txt", "r") # Day 1 Part 2
 top_3 = [0, 0, 0]
-
 count = 0
 for line in file:
-    line = line.strip("\n")
-    if line == "":
+    if line.strip("\n") == "":
         if min(top_3) < count: top_3[top_3.index(min(top_3))] = count
         count = 0
     else: count = count + int(line)
-        
 print(sum(top_3))
